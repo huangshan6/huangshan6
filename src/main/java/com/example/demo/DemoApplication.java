1e0fc5508dcc51c68076ea5703dce089ff5ff8e0
zangqingran <568172115@qq.com>
2024-06-21 15:21:50 +0800

    erro

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
@@ -11,6 +12,7 @@ import java.net.UnknownHostException;
 
 @Slf4j
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.demo.zangqingran.entity")
 public class DemoApplication {
 
 	public static void main(String[] args) throws UnknownHostException {
