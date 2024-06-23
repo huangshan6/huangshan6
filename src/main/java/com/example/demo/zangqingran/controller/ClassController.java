3dba5af8b8021dc1cb5e91b8a869954bc398c84b
zangqingran <568172115@qq.com>
2024-06-23 21:31:29 +0800

    mysql增删改查版本

--- src/main/java/com/example/demo/zangqingran/controller/ClassController.java
+++ src/main/java/com/example/demo/zangqingran/controller/ClassController.java
@@ -5,13 +5,14 @@ import com.example.demo.zangqingran.repository.ClassRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class ClassController {
 
-    @Autowired
+    @Resource
     ClassRepository classesRepository;
 
     @GetMapping("/api/classes/{id}")
@@ -28,23 +29,24 @@ public class ClassController {
         return  list;
     }
     //处理"/classes/"的POST请求，用来创建classes
-    @PostMapping("/api/stdent/add")
-    public String addclasses( @RequestBody ClassRoom classes ) {
+    @PostMapping("/api/classes/add")
+    public String addclasses( @RequestParam int classID,@RequestParam String className,@RequestParam String grade,@RequestParam String classTeacherName ) {
+        ClassRoom classes = new ClassRoom(classID,className,grade,classTeacherName);
         classesRepository.save(classes);
         return "success";
     }
     //处理"/classes/{id}"的PUT请求，用来更新classes信息
-    @PutMapping("/{id}")
-    public String updateclasses( @PathVariable int id, @RequestBody ClassRoom classes ) {
-        ClassRoom classes_ = classesRepository.findById(id).get();
-        classes_.setClassName(classes.getClassName());
-        classes_.setGrade(classes.getGrade());
-        classes_.setClassTeacherName(classes.getClassTeacherName());
-        classesRepository.save(classes_);
+    @PutMapping("/api/classes/edit/{id}/{className}/{grade}/{classtn}")
+    public String updateclasses( @PathVariable int id,@PathVariable String className,@PathVariable String grade,@PathVariable String classtn) {
+        ClassRoom classes = classesRepository.findById(id).get();
+        classes.setClassName(className);
+        classes.setGrade(grade);
+        classes.setClassTeacherName(classtn);
+        classesRepository.save(classes);
         return "success";
     }
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable int id) {
+    @DeleteMapping("/api/classes/delete/{id}/{className}/{grade}/{classtn}")
+    public String delete(@PathVariable int id,@PathVariable String className,@PathVariable String grade,@PathVariable String classtn) {
         classesRepository.deleteById(id);
         return "success";
     }
